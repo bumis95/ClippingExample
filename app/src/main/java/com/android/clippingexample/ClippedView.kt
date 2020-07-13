a793de3a7ea625575a8cc413ff5c65a8f7803151
bumis95 <bumis95@gmail.com>
2020-07-13 15:01:15 +0300

    Create the ClippedView class

---
+++ app/src/main/java/com/android/clippingexample/ClippedView.kt
@@ -0,0 +1,11 @@
+package com.android.clippingexample
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ClippedView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attrs, defStyle)
